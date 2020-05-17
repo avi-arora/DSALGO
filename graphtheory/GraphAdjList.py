c71a5d60e0feed68111b35ecb7a6f6700aad868c
avishek arora <avishek.arora@icloud.com>
2020-05-18 03:39:27 +0530

    Basic: Added Linear topological sorting.

--- graphtheory/GraphAdjList.py
+++ graphtheory/GraphAdjList.py
@@ -2,82 +2,117 @@ class Vertex:
     def __init__(self, label):
         self.label, self.visited = label, False
         self.neighbors = []
-        #stores pre and post visit values
+        # stores pre and post visit values
         self.pre, self.post = 0, 0
-    
-    #to add a neighbor
+
+    # to add a neighbor
     def addNeighbor(self, neighbor):
         if neighbor not in self.neighbors:
             self.neighbors.append(neighbor)
-    
+
+    def removeNeighbor(self, nbr):
+        """removes a neighbor from the vertex"""
+        deletedNbr = None
+        if nbr in self.neighbors:
+            deletedNbr = self.neighbors.remove(nbr)
+        return deletedNbr
+
     def getConnections(self):
         return self.neighbors
-    
+
     def getVertexLabel(self):
         return self.label
 
     def visit(self):
         self.visited = True
-    
+
     def preVisit(self, value):
         self.pre = value
-    
+
     def postVisit(self, value):
         self.post = value
 
     def getPre(self):
         return self.pre
-    
+
     def getPost(self):
         return self.post
-    
+
     def isVisited(self):
         return self.visited
 
     def getLabel(self):
         return self.label
-    
+
+
 class Graph:
     def __init__(self):
         self.graph = {}
-    
-    def __getitem__(self,label):
-        return self.graph[label]
-    
-    def totalVertices(self):
-        return len(self.graph.keys())
-    
-    def addVertex(self, label):
-        self.graph[label] = Vertex(label)
-    
-    def getVertices(self):
-        return self.graph.keys()
-    
-    def getVertex(self,label):
+
+    def __getitem__(self, label):
         return self.graph[label]
 
-    def addEdge(self, fromLabel, toLabel,isDirected=False):
+    def totalVertices(self):
+        return len(self.graph.keys())
+
+    def addVertex(self, label):
+        self.graph[label] = Vertex(label)
+
+    def removeVertex(self, label):
+        if label in self.graph:
+            # delete from main graph
+            self.graph.pop(label)
+            # delete all neighbors
+            for _, vertex in self.graph.items():
+                vertex.removeNeighbor(label)
+
+    def getVertices(self):
+        return self.graph.keys()
+
+    def getVertex(self, label):
+        return self.graph[label]
+
+    def getOutDegree(self, label):
+        """
+        returns the outdegree of a vertex
+        works only with Directed Edges, useless in other edges
+        """
+        return len(self.graph[label].getConnections())
+
+    def getInDegree(self, label):
+        """
+        returns the InDegree of a vertex, 
+        works only with directed edges, useless in undirected graphs.
+        also considers loops in vertex
+        """
+        inDegree = 0
+        for vertex in self.graph:
+            if label in self.graph[vertex].getConnections():
+                inDegree += 1
+        return inDegree
+
+    def addEdge(self, fromLabel, toLabel, isDirected=False):
         """ This method also consider the edge cases
             if fromLabel and toLabel isn't a vertex yet, 
             it'll add them to vertex list
         """
-        #handle corner cases listed in description
+        # handle corner cases listed in description
         if fromLabel not in self.getVertices():
             self.addVertex(fromLabel)
         if toLabel not in self.getVertices():
             self.addVertex(toLabel)
-        #add the edge
+        # add the edge
         self.graph[fromLabel].addNeighbor(toLabel)
-        #handle undirected graph case
+        # handle undirected graph case
         if not isDirected:
             self.graph[toLabel].addNeighbor(fromLabel)
-    
+
     def getEdges(self):
         edges = []
         for u in self.getVertices():
             individual = []
             for e in self.graph[u].getConnections():
-                individual.append([u,e])
+                individual.append([u, e])
             edges.append(individual)
         return edges
 
@@ -85,6 +120,7 @@ class Graph:
         for v in self.getVertices():
             print(v)
             print(self.graph[v].getConnections())
+
 
 def SampleGraph_1():
     G = Graph()
@@ -96,31 +132,25 @@ def SampleGraph_1():
     G.addVertex('f')
     G.addVertex('g')
     G.addVertex('h')
-    
-    G.addEdge('a','b')
-    G.addEdge('b','c')
-    G.addEdge('c','d')
-    G.addEdge('c','e')
-    G.addEdge('f','e')
-    G.addEdge('e','g')
-    G.addEdge('e','h')
-    G.addEdge('b','h')
-    
+
+    G.addEdge('a', 'b')
+    G.addEdge('b', 'c')
+    G.addEdge('c', 'd')
+    G.addEdge('c', 'e')
+    G.addEdge('f', 'e')
+    G.addEdge('e', 'g')
+    G.addEdge('e', 'h')
+    G.addEdge('b', 'h')
+
     return G
-    
+
 
 if __name__ == '__main__':
     G = Graph()
     G.addVertex('a')
     G.addVertex('b')
     G.addVertex('c')
-    G.addEdge('a','b')
-    G.addEdge('a','c')
+    G.addEdge('a', 'b')
+    G.addEdge('a', 'c')
     G.printGraph()
     print(G.getEdges())
-
-
-
-        
-
-
--- graphtheory/TopologicalSort.py
+++ graphtheory/TopologicalSort.py
@@ -1,4 +1,5 @@
 from GraphAdjList import Graph, Vertex
+import copy
 
 
 def TopologicalSort(G: Graph):
@@ -24,19 +25,61 @@ def TopologicalSort(G: Graph):
     return sorted[::-1]
         
 
+def TopologicalSortLinear(G: Graph):
+    """
+    Returns topological sorted vertex list of graph G, 
+    uses linear approach, relies upon queues and InDegree Computation approach. 
+    TimeComplexity: O(V+E), same as recursive with backtracking one (efficient algorithm as well)
+    SpaceComplexity: ?
+    """
+    #deep copy the original graph
+    #not necessary needed in sample programs
+    G = copy.deepcopy(G)
+    sorted, queue = [], []
+
+    #add zero-InDegree vertices in queue, initial step
+    for vertex in G.getVertices():
+        if G.getInDegree(vertex) == 0:
+            #add in queue
+            queue.append(vertex)
+    
+    #zero InDegree items in queue are starting point of algorithm. 
+    #run the algorithm while queue is not empty
+    while queue:
+        zeroVertex = queue.pop(0)
+        sorted.append(zeroVertex)
+        #remove the zero vertex from the graph
+        G.removeVertex(zeroVertex)
+        #re-calculate the InDegree of vertex and enqueue the one with 0 InDegree
+        for vertex in G.getVertices():
+            if G.getInDegree(vertex) == 0 and vertex not in queue:
+                queue.append(vertex)
+        
+    return sorted
+ 
 
 
 def TopologicalSortBruteForce(G: Graph):
     """
     un-optimized version of topological sorting
     """
-    sorted = []
-    for vertex in G.getVertices():
-        pass
+    pass
+
          
 
 if __name__ == "__main__":
     G = Graph()
+    #G.addVertex("A")
+    #G.addVertex("B")
+    #G.addVertex("C")
+    #G.addVertex("D")
+    #G.addVertex("E")
+
+    #G.addEdge("A", "B", True)
+    #G.addEdge("B", "C", True)
+    #G.addEdge("C", "D", True)
+    #G.addEdge("A", "E", True)
+    
     G.addVertex("2")
     G.addVertex("3")
     G.addVertex("5")
@@ -57,4 +100,4 @@ if __name__ == "__main__":
     G.addEdge("11","2", True)
 
     print(TopologicalSort(G)) 
-    
+    print(TopologicalSortLinear(G))
