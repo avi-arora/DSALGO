15bfc86ddeee7a119e43caab0ae2a60e158c24de
avishek arora <avishek.arora@icloud.com>
2020-07-02 03:59:41 +0530

    added codeforces round 654 div 2

--- linkedlist/DetectCycle.py
+++ linkedlist/DetectCycle.py
@@ -1,18 +1,77 @@
 from SinglyLinkedList import SinglyLinkedList
 
+
 def hasCycle(list: SinglyLinkedList):
     """
     Detect a cycle using floyd 2 pointer approach
+    TC: O(n)
+    SC: O(1)
     """
-    temp = list
-    fastTemp = list
-    while temp:
-        temp = temp.getNext()
-        if fastTemp.getNext().getNext():
-            fastTemp = fastTemp.getNext().getNext()
-        if temp == fastTemp:
+    slow = fast = list
+    while fast and fast.getNext() and fast.getNext().getNext():
+        slow, fast = slow.getNext(), fast.getNext().getNext()
+        if slow is fast:
             return 1
     return 0
+
+
+def hasCycleMemory(list: SinglyLinkedList):
+    """
+    uses hash table to check the cycle
+    TC: O(n)
+    SC: O(n)
+    """
+    visited, curr = {}, list
+    while curr:
+        if curr in visited:
+            return 1
+        visited[curr], curr = curr.getData(), curr.getNext()
+
+    return 0
+
+
+def hasCycleWithLength(list: SinglyLinkedList):
+    """
+    Returns the length of the cycle if any
+    TC: O(n)
+    SC: O(1)
+    """
+    slow = fast = list
+    isCyclic = False
+    while fast and fast.getNext() and fast.getNext().getNext():
+        slow, fast = slow.getNext(), fast.getNext().getNext()
+        if slow is fast:
+            # overlapping detected, i.e cycle exits
+            isCyclic = True
+            break
+
+    cycleLen = 0
+    if isCyclic:
+        slow = slow.getNext()
+        while slow is not fast:
+            cycleLen, slow = cycleLen + 1, slow.getNext()
+
+    return cycleLen
+
+def hasCycleWithLocation(list: SinglyLinkedList):
+    """
+    Returns the node where the cycle began
+    TC: O(n)
+    SC: O(1)
+    """
+    #detect a cycle
+    slow = fast = list
+    isCyclic = False
+    while fast and fast.getNext() and fast.getNext().getNext():
+        slow, fast = slow.getNext(), fast.getNext().getNext()
+        if slow is fast:
+            isCyclic = True
+            break
+    
+            
+
+def hasCycleBruteForce(list: SinglyLinkedList):
+    pass
 
 
 if __name__ == "__main__":
@@ -26,10 +85,9 @@ if __name__ == "__main__":
 
     l.print()
 
-    #modify list to create a cycle
-    l.getNext().getNext().setNext(l.getNext())
+    # modify list to create a cycle
+    #l.getNext().getNext().getNext().setNext(l.getNext())
 
   #  l.print()
 
-    print(hasCycle(l))
-    
+    print(hasCycleWithLength(l))
